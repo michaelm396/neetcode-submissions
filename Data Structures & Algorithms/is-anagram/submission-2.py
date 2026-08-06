b3efb0e2a27baf093d25c2137b1808961cafd8a2
Michael Moorer Jr. <michael.moorer396@gmail.com>
2026-08-05 17:15:50 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict1 = {}
+        dict2 = {}
+        for value in s:
+            if value in dict1:
+                sub = dict1[value]
+                sub = sub + 1
+                dict1[value] = sub
+            if value not in dict1: 
+                dict1[value] = 1
+        
+        for value in t:
+            if value in dict2:
+                sub = dict2[value]
+                sub = sub + 1
+                dict2[value] = sub
+            if value not in dict2: 
+                dict2[value] = 1
+        print(dict1)
+        print(dict2)
+        if dict1 == dict2:
+            return True
+        else:
+            return False
